f2471eda756c23b0c6d55ba91061805c55e92b10
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-05-12 14:48:03 +0530

    initial commit

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import {Box} from '@mui/system'
 
 export default function Home() {
 
-  
-
   return (
-    <></>
+    <Box sx={{minHeight:'100vh', width:'100vw'}} >
+      <Box sx={{display:'flex',justifyContent:'center'}} >
+
+      </Box>
+    </Box>
   )
 }
