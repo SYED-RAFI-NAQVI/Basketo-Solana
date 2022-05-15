d6cc0b9222e456e43d7534713e81fc1d9f4ac798
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2022-05-15 12:12:53 +0530

    ah

--- site/components/create/RatioChart.js
+++ site/components/create/RatioChart.js
@@ -24,9 +24,9 @@ const RatioChart = ({data}) => {
             fill="#8884d8"
             label
           />
-          {data.map((entry, index) => (
+          {/* {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
+            ))} */}
           <Tooltip />
         </PieChart>
       </ResponsiveContainer>
